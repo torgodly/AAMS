2bae4fa89e65ae217120767adce7cf6d19cca5d9
torgodly <torgodly@gmail.com>
2023-05-25 12:51:17 +0200

    tranltion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,9 +21,9 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'type' => 'admin',
         ]);
-        \App\Models\User::factory(10)->create(['type' => 'Teacher']);
-        \App\Models\User::factory(2)->create(['type' => 'Student', 'group_id' => 1]);
-        Group::factory()->create(['teacher_id' => 2]);
+//        \App\Models\User::factory(10)->create(['type' => 'Teacher']);
+//        \App\Models\User::factory(2)->create(['type' => 'Student', 'group_id' => 1]);
+//        Group::factory()->create(['teacher_id' => 2]);
 //        $teachers = Teacher::all();
 //        foreach ($teachers as $teacher) {
 //            Group::factory()->create(['teacher_id' => $teacher->id]);
--- resources/views/livewire/group/create.blade.php
+++ resources/views/livewire/group/create.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex justify-end" x-data="{show: false}">
-        <x-primary-button @click="show = true">Create Group</x-primary-button>
+        <x-primary-button @click="show = true">{{__('Create Group')}}</x-primary-button>
         <div x-show="show"  class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
 
             <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
@@ -10,8 +10,17 @@
 
                     <div
                         class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6" >
+                        <div class="absolute top-0 right-0 pt-4 pr-4">
+                            <button @click='show = false' type="button" class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150"
+                                    aria-label="Close">
+                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M6 18L18 6M6 6l12 12"/>
+                                </svg>
+                            </button>
+                        </div>
                         <div>
-                            <h1 class="text-2xl font-semibold text-gray-900">Create Group</h1>
+                            <h1 class="text-2xl font-semibold text-gray-900">{{__('Create Group')}}</h1>
                         </div>
                         <form action="{{route('groups.store')}}" method="post">
                             @csrf
@@ -23,7 +32,7 @@
                             </div>
                             <div class="mt-4">
                                 <x-input-label for="teacher" class="text-md" :value="__('Teacher')"/>
-                                <x-select-input label="teacher" name="teacher_id"
+                                <x-select-input label="Teacher" name="teacher_id"
                                                 class="block mt-1 w-full"
                                                 :options="$teachers"/>
                                 <x-input-error class="mt-2" :messages="$errors->get('teacher_id')"/>
